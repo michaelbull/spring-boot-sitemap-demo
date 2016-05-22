536cb625979be0fcb16a58a29c870dc0fa9821cd
Michael Bull <mikebull94@gmail.com>
2016-05-22 17:43:22 +0100

    Add save method to BlogRepository

--- src/main/java/com/mikebull94/demo/sitemap/model/BlogRepository.java
+++ src/main/java/com/mikebull94/demo/sitemap/model/BlogRepository.java
@@ -13,8 +13,12 @@ public final class BlogRepository {
 	private final List<BlogEntry> blogEntries = new ArrayList<>();
 
 	public BlogRepository() {
-		blogEntries.add(new BlogEntry("an-old-blog-post", "Some content...", LocalDate.of(2014, Month.DECEMBER, 1)));
-		blogEntries.add(new BlogEntry("a-newer-blog-post", "Some content...", LocalDate.of(2016, Month.MAY, 22)));
+		save(new BlogEntry("an-old-blog-post", "Some content...", LocalDate.of(2014, Month.DECEMBER, 1)));
+		save(new BlogEntry("a-newer-blog-post", "Some content...", LocalDate.of(2016, Month.MAY, 22)));
+	}
+
+	public void save(BlogEntry entry) {
+		blogEntries.add(entry);
 	}
 
 	public List<BlogEntry> findAll() {
